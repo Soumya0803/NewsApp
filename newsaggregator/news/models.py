ff390835a570d6af02b7b70064a0f3b8d04e4f91
Soumya0803 <soumyachopra@gmail.com>
2023-07-11 15:16:07 +0530

    Minor changes in news model

--- newsaggregator/news/models.py
+++ newsaggregator/news/models.py
@@ -9,14 +9,15 @@ CATEGORY_CHOICES = [
     ('sports','Sports'),
     ('technology','Technology')
 ]
+
 class News(models.Model):
-    source_id = models.CharField(max_length=100)
+    source_id = models.CharField(max_length=100, null= True)
     source_name =  models.CharField(max_length=100)
-    author =  models.CharField(max_length=100)
-    title =  models.CharField(max_length=100)
-    description =  models.CharField(max_length=255)
-    url = models.URLField(max_length=200, null=True, blank=True)
-    urlToImage = models.URLField(max_length=200, null=True, blank=True)
+    author =  models.CharField(max_length=100, null= True)
+    title =  models.CharField(max_length=500)
+    description =  models.CharField(max_length=500)
+    url = models.URLField(max_length=500, null=True, blank=True)
+    urlToImage = models.URLField(max_length=500, null=True, blank=True)
     publishedAt = models.DateTimeField()
     content = models.TextField()
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
