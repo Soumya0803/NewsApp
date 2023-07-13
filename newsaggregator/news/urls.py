6be30c3b8fc79b93c16f23accb720d02ab1761f8
Soumya0803 <soumyachopra@gmail.com>
2023-07-13 17:48:21 +0530

    Add class based view and pagination in  home page

--- newsaggregator/news/urls.py
+++ newsaggregator/news/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import top_headlines
+from .views import TopHeadlinesView
 urlpatterns = [
-    path('', top_headlines, name='home'),
+    path('', TopHeadlinesView.as_view(), name='top_headlines'),
 ]
