70b344ed14a9b5456f682daaecd2469ac9e7824c
Soumya <soumyachopra@gmail.com>
2023-07-14 03:46:43 +0530

    CSRF

--- newsaggregator/newsaggregator/settings.py
+++ newsaggregator/newsaggregator/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-from decouple import config
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = [
     'https://newsapp.soumya0803.in'
 ]
 
-
+CSRF_TRUSTED_ORIGINS = ['127.0.0.1', 'https://newsapp.soumya0803.in']
 # Application definition
 
 INSTALLED_APPS = [
@@ -83,10 +83,10 @@ WSGI_APPLICATION = 'newsaggregator.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'), 
+        'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'), 
+        'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT')
     }
 }
@@ -133,6 +133,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CELERY_RESULT_BACKEND = "django-db"
-CELERY_BROKER_URL = config('CELERY_BROKER_REDIS_URL', default='redis://localhost:6379')
-CELERY_BROKER_URL = config('CELERY_BROKER_REDIS_URL', default='redis://localhost:6379')
+CELERY_BROKER_URL = config('CELERY_BROKER_REDIS_URL',
+                           default='redis://localhost:6379')
+CELERY_BROKER_URL = config('CELERY_BROKER_REDIS_URL',
+                           default='redis://localhost:6379')
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers.DatabaseScheduler'
